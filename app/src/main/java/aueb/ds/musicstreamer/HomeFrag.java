4b2f54e9258fb489a459cd1e895d05279d4f2122
kkoliou <56164783+kkoliou@users.noreply.github.com>
2020-07-28 12:26:16 +0300

    Options menu added in HomeFrag

--- app/src/main/java/aueb/ds/musicstreamer/HomeFrag.java
+++ app/src/main/java/aueb/ds/musicstreamer/HomeFrag.java
@@ -2,11 +2,10 @@ package aueb.ds.musicstreamer;
 
 import android.content.Intent;
 import android.os.Bundle;
-
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.appcompat.widget.Toolbar;
 import androidx.fragment.app.Fragment;
-
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -15,6 +14,7 @@ import android.widget.EditText;
 public class HomeFrag extends Fragment {
 
     private EditText editClick;
+    private Toolbar toolbar;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
@@ -25,6 +25,8 @@ public class HomeFrag extends Fragment {
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
         editClick = view.findViewById(R.id.editTxt);
+        toolbar = view.findViewById(R.id.toolbar);
+        toolbar.inflateMenu(R.menu.options_menu);
 
         editClick.setOnClickListener(new View.OnClickListener() {
             @Override
